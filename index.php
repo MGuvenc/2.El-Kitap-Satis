b1f3ab812c1c448517b12853112b62ec1de020fb
DeryaKesgin <99399615+DeryaKesgin@users.noreply.github.com>
2023-01-31 21:26:46 +0300

    Update index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<html lang="TR">
+<head>
+    <title>2.El Kitap</title>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+    <header>
+        <h1>2.EL KITAP SATISI</h1>
+    </header>
+
+    <ul> 
+         <li><a href="index.php">Ana Sayfa</a></li>
+         <li><a href="iletisim.php">İletişim</a></li> 
+         <li><a href="sepet.php">Sepet</a></li> 
+         <li><a href="#"><?php echo $_SESSION["isim"]; ?></a></li>
+         <li><a href="cikis.php">Çıkış</a></li>
+     </ul> 
+  
+
+    <nav>
+        <h3>KATEGORİLER</h3>
+        
+    </nav>
+
+    <section>
+       
+    </section>
+    <footer>MGuvenc</footer>
+</body>
+</html>
