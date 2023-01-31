3c36709ee971f319923be664dfabc618a62e6cb0
Murat GÜVENÇ <muratguvenc.py@gmail.com>
2023-01-31 03:50:59 +0300

    Create db.sql

---
+++ sql/db.sql
@@ -0,0 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 31 Oca 2023, 03:50:10
+-- Sunucu sürümü: 10.4.27-MariaDB
+-- PHP Sürümü: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `kitapsatis`
+--
+CREATE DATABASE IF NOT EXISTS `kitapsatis` DEFAULT CHARACTER SET utf8 COLLATE utf8_turkish_ci;
+USE `kitapsatis`;
+
+
+-- --------------------------------------------------------
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
